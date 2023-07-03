bf490a9b5808ced16146f75eb64105ca869c03b7
mamunurrashid1010 <mamunurrashid1010@gmail.com>
2023-07-03 12:15:25 +0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Real Estate App</h1>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name:'App',
+  data(){
+    return{
+
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
