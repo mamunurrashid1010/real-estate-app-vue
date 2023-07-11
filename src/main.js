52dfed3abb1732562ce3a9ce5257e67b6a49118e
mamunurrashid1010 <mamunurrashid1010@gmail.com>
2023-07-11 10:43:28 +0600

    create route file and import, use in main.js

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import './style.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import App from './App.vue'
+import router from "./router/index.js";
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
