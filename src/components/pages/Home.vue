0fcafd7b12b998ca0cb07e863d4f14528f658f90
mamunurrashid1010 <mamunurrashid1010@gmail.com>
2023-07-12 15:53:15 +0600

    Home : create home page

---
+++ src/components/pages/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Home Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data(){
+    return{
+
+    }
+  },
+  methods:{
+  }
+}
+</script>
+
+<style>
+</style>
