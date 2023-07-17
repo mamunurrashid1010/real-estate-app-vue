9117ab59d6492b1be97d5c18070e598dd4870bab
mamunurrashid1010 <mamunurrashid1010@gmail.com>
2023-07-17 10:46:26 +0600

    style.css add and root,header,nav css add

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-// import './style.css'
+import './style.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import App from './App.vue'
 import router from "./router/index.js";
