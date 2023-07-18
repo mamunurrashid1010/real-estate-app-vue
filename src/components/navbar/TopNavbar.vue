de9d7cf6ec7825e04a61043dc55be6356b99bf21
mamunurrashid1010 <mamunurrashid1010@gmail.com>
2023-07-18 10:58:42 +0600

    Navbar router-link add

--- src/components/navbar/TopNavbar.vue
+++ src/components/navbar/TopNavbar.vue
@@ -1,6 +1,24 @@
 <template>
   <div>
-      <h4>Navbar</h4>
+    <header>
+
+      <router-link to="/" class="logo"><span>real</span>Estate</router-link>
+
+      <nav class="navbar">
+        <router-link to="/">home</router-link>
+        <router-link to="/">services</router-link>
+        <router-link to="/">featured</router-link>
+        <router-link to="/">agents</router-link>
+        <router-link to="/">contact</router-link>
+      </nav>
+
+      <div class="icons">
+        <div id="menu-bars" class="fas fa-bars"></div>
+        <router-link to="/" class="fas fa-heart"></router-link>
+        <router-link to="/" class="fas fa-user"></router-link>
+      </div>
+
+    </header>
   </div>
 </template>
 
