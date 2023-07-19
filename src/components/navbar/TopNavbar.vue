6b1a873c359c3c822ea2a4fc527ec3545ba90242
mamunurrashid1010 <mamunurrashid1010@gmail.com>
2023-07-19 11:26:16 +0600

    navbar : mobile navbar expand implement

--- src/components/navbar/TopNavbar.vue
+++ src/components/navbar/TopNavbar.vue
@@ -4,7 +4,7 @@
 
       <router-link to="/" class="logo"><span>real</span>Estate</router-link>
 
-      <nav class="navbar">
+      <nav class="navbar" :class="navBarActive">
         <router-link to="/">home</router-link>
         <router-link to="/">services</router-link>
         <router-link to="/">featured</router-link>
@@ -13,7 +13,7 @@
       </nav>
 
       <div class="icons">
-        <div id="menu-bars" class="fas fa-bars"></div>
+        <div id="menu-bars" class="fas fa-bars" @click="mobileNavExpand()"></div>
         <router-link to="/" class="fas fa-heart"></router-link>
         <router-link to="/" class="fas fa-user"></router-link>
       </div>
@@ -27,10 +27,20 @@ export default {
   name:'TopNavbar',
   data(){
     return{
-
+        navBarActive:'inactive'
     }
   },
   components:{
+
+  },
+  methods:{
+    mobileNavExpand(){
+      if (this.navBarActive == 'inactive')
+        this.navBarActive = 'active';
+      else
+        this.navBarActive = 'inactive';
+
+    }
 
   }
 }
