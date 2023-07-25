599166ada9be7b4e1491c90865d4441f868161f5
mamunurrashid1010 <mamunurrashid1010@gmail.com>
2023-07-25 11:01:03 +0600

    Home page : feature component implement

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -2,16 +2,18 @@
   <div>
     <Banner/>
     <Service/>
+    <Feature/>
   </div>
 </template>
 
 <script>
 import Banner from "../component/home/Banner.vue";
 import Service from "../component/home/Service.vue";
+import Feature from "../component/home/Feature.vue";
 
 export default {
   name: 'Home',
-  components: {Service, Banner},
+  components: {Feature, Service, Banner},
   data(){
     return{
 
