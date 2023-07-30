e7391d6e2f8fc4e6ae638588c7cde30fe4528b19
mamunurrashid1010 <mamunurrashid1010@gmail.com>
2023-07-30 11:08:20 +0600

    Home page : contact component create

---
+++ src/components/component/home/Contact.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Contact',
+  data(){
+    return{
+
+    }
+  },
+  methods:{
+  }
+}
+</script>
+
+<style>
+
+</style>
